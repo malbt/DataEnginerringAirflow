ebe50bea26fe9ef8df198e074e98a15d333cb232
Maleda Tessema <mal.bt27@gmail.com>
2020-04-04 23:00:28 -0400

    troubleshooting connection to sql engine

--- getdata.py
+++ getdata.py
@@ -36,7 +36,7 @@ get_data = BashOperator(
 
 # create a sql engine
 def create_engine():
-    mysql_engine = sqlalchemy.create_engine('mysql+pymysql://root:zipcoder@localhost/airflow_db')
+    mysql_engine = sqlalchemy.create_engine('mysql+pymysql://root:zipcoder@localhost:3306/airflow_db')
     return mysql_engine
 
 
@@ -66,3 +66,13 @@ tasks
 get_data
 create_engine >> load_data
 """
+
+"""
+only one task has been working so far, 
+i am creating separate dags for the tasks to figure out what works best
+i haven't been commiting my work because i had to create a separate .py file
+because i haven't been able to integrate the file's i wanted in to my airflow, 
+i did clone my project inside the airflow dir, but i had to move the file into the dag.
+
+"""
+
