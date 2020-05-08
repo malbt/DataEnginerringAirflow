32e83292f24b2244d2ada78e3c346b8d1025fc9a
Maleda Tessema <mal.bt27@gmail.com>
2020-05-07 23:01:18 -0400

    "python operator edit"

--- tbl_operators.py
+++ tbl_operators.py
@@ -11,61 +11,61 @@ t10 = PythonOperator(
     dag=dag,
 )
 
-t10 = PythonOperator(
+t11 = PythonOperator(
     task_id='create_table_from_merge2',
     provide_context=False,
     python_callable=create_table_from_merge2,
     dag=dag,
 )
-t11 = PythonOperator(
+t12 = PythonOperator(
     task_id='create_table_from_merge3',
     provide_context=False,
     python_callable=create_table_from_merge3,
     dag=dag,
 )
-t12 = PythonOperator(
+t13 = PythonOperator(
     task_id='create_table_from_merge4',
     provide_context=False,
     python_callable=create_table_from_merge4,
     dag=dag,
 )
-t13 = PythonOperator(
+t14 = PythonOperator(
     task_id='create_table_from_merge5',
     provide_context=False,
     python_callable=create_table_from_merge5,
     dag=dag,
 )
-t14 = PythonOperator(
+t15 = PythonOperator(
     task_id='create_table_from_merge6',
     provide_context=False,
     python_callable=create_table_from_merge6,
     dag=dag,
 )
-t15 = PythonOperator(
+t16 = PythonOperator(
     task_id='create_table_from_merge7',
     provide_context=False,
     python_callable=create_table_from_merge7,
     dag=dag,
 )
-t16 = PythonOperator(
+t17 = PythonOperator(
     task_id='create_table_from_merge8',
     provide_context=False,
     python_callable=create_table_from_merge8,
     dag=dag,
 )
-t17 = PythonOperator(
+t18 = PythonOperator(
     task_id='create_table_from_merge9',
     provide_context=False,
     python_callable=create_table_from_merge9,
     dag=dag,
 )
-t18 = PythonOperator(
+t19 = PythonOperator(
     task_id='create_table_from_merge10',
     provide_context=False,
     python_callable=create_table_from_merge10,
     dag=dag,
 )
-t19 = PythonOperator(
+t20 = PythonOperator(
     task_id='create_table_from_merge11',
     provide_context=False,
     python_callable=create_table_from_merge11,
